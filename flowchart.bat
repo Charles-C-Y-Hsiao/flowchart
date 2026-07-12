9486ada6a95ec50fdebcf83643321f3fefe061ef
Charles C Y Hsiao <charles@example.com>
2026-07-13 01:31:05 +0800

    Remove ignored local files from git

--- flowchart.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title flowchart: 3013
-cd C:\Web_Practice\Function\flowchart
-echo C:\Web_Practice\Function\flowchart
-echo Starting server.js ...
-start "" cmd /c "timeout /t 2 >nul && start chrome http://localhost:3013"
-node server.js
-pause
